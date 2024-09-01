443f4c8c815dd229475e1b827822f25895380338
Yukta Bhatia <yuktabhatia8@gmail.com>
2024-09-01 08:49:07 +0530

    Added scripts to run and build in package.json

--- App.js
+++ App.js
@@ -1,8 +1,3 @@
-const heading = React.createElement(
-  "h1",
-  { id: "heading" }, //attribute
-  "Hello World from React"
-);
-console.log(heading); //object
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+import React from "react"
+import ReactDOM from "react-dom/client"
+
